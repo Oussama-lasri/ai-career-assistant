ccf7a001a7b200ed5da6a1cd361b9e2aa0bb40c4
ousama lasri <oussama.lasri.2k@gmail.com>
2025-07-14 19:42:49 +0100

    feat: create user model

---
+++ Back-end/api/models/User.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from ..core.database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    full_name = Column(String, nullable=True)
+    disabled = Column(String, default=False)
+    created_at = Column(DateTime, nullable=False,default=datetime.now)
