6524f469e65ffb294b235464fcc45ec3e8f9d2ff
ousama lasri <oussama.lasri.2k@gmail.com>
2025-07-14 19:42:28 +0100

    feat: config db

---
+++ Back-end/api/core/database.py
@@ -0,0 +1,31 @@
+import os
+from typing import Annotated
+from dotenv import load_dotenv
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker , Session
+
+
+
+
+
+
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+
+DbSession = Annotated[Session, Depends(get_db)]
