e2114a665060a35c202828d7648e16ef9de97003
ousama lasri <oussama.lasri.2k@gmail.com>
2025-07-15 18:58:13 +0100

    feat(auth) : create service for authentication process that include all the function we need

---
+++ Back-end/api/services/authentication_service.py
@@ -0,0 +1,47 @@
+
+
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends , HTTPException, status
+from sqlalchemy.orm import Session
+from ..repositories.user_repository import UserRepository
+from ..schemas.User_schema import UserCreate , UserInDB , User as UserSchema
+from ..models.User import User
+
+
+class authenticationService:
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        self.oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/authentication/login")
+    
+    
+    def authenticate_user(self, db: Session, username: str, password: str):
+        user_repository = UserRepository(db)
+        
+        pass
+
+    def create_user(self, db: Session, user: UserCreate):
+        new_user = User(**user.model_dump(exclude={"password"}))
+        new_user.hashed_password = self.get_password_hash(user.password.get_secret_value())
+
+        if self.get_user(db,user.email) is not None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered",
+            )
+        user_repository = UserRepository(db)
+        created_user = user_repository.create_user(new_user)
+
+        # Conversion ORM → Pydantic
+        return UserSchema.model_validate(created_user)
+
+    def get_password_hash(self,password):
+        return self.pwd_context.hash(password)
+    
+    def get_user(self,db: Session ,email: str ):
+        user_repository = UserRepository(db)
+        user_dict = user_repository.get_user_by_email(email)
+        if user_dict is None:
+            return None 
+
+        return UserInDB.model_validate(user_dict)
